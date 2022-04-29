ef81665fe09a38a876a5c80ebc5b966aa184f5c5
Int <57096811+huanginch@users.noreply.github.com>
2022-04-30 00:13:06 +0800

    upload files of EX6

---
+++ EX6/EX6.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NAME_LENGTH 1000
+
+//circular linked list
+struct circle{
+	char name[NAME_LENGTH + 1];
+	struct circle *next;
+	struct circle *prior;
+};
+
+void create_list(struct circle **first); //create the circular linked list
+void add_person(struct circle **first, struct circle **End, char name[]);//add people to the circular linked list
+void kill(struct circle **first); //kill people
+
+int main(void){
+	
+	struct circle *head = NULL;
+	create_list(&head);
+	kill(&head);
+
+	return 0;
+}
+
+void create_list(struct circle **first){
+	int i = 0;//use for the loop
+	int n = 0; //the number of the courses
+	scanf("%d\n", &n);
+	
+	struct circle *end;
+
+	//create the linked list
+	for(i = 0; i < n; i++){
+		char name[NAME_LENGTH + 1];//name of the course
+
+		scanf("%s", name);
+		add_person(first, &end, name);
+	}
+	
+	//circle the list
+	(*first)->prior = end;
+	end->next = *first;
+}
+
+void add_person(struct circle **first, struct circle **End, char name[]){
+
+	//create new course node
+	struct circle *new_person = malloc(sizeof(struct circle));
+	strncpy(new_person->name, name, strlen(name));
+	new_person->next = NULL;
+	new_person->prior = NULL;
+
+
+	if(*first == NULL){// if there is no node in the list
+		*first = new_person;
+		*End = *first;
+	}
+	else{
+		//add new person the the end
+		(*End)->next = new_person;
+		new_person->prior = *End;
+		*End = (*End)->next;
+	}
+}
+
+void kill(struct circle **first){
+	int k; //steps
+	char *direction;
+	int i;
+
+	fflush(stdin);
+	scanf("%d %s", &k, direction);
+
+	struct circle *current = *first;//the current person
+	struct circle *prev = NULL;//the previous person
+	
+	while(current->next != current){
+		if(!strcmp(direction, "CLOCKWISE")){
+
+			//kill from clockwise
+
+			for(i = 0; i < k; i++){
+				//find the kill target
+				prev = current;
+				current = current->next;
+			}
+
+			prev->next = current->next;
+			printf("%s KILLED\n", current->name);
+			free(current);
+			current = prev->next;
+			current->prior = prev;
+		}
+		else if(!strcmp(direction, "COUNTERCLOCKWISE")){
+
+			for(i = 0; i < k; i++){
+				prev = current;
+				current = current->prior;
+			}
+
+			prev->prior = current->prior;
+			printf("%s KILLED\n", current->name);
+			free(current);
+			current = prev->prior;
+			current->next = prev;
+		}
+		else{
+			printf("wrong direction\n");
+		}
+	}
+
+	printf("%s SURVIVE\n", current->name);
+
+}
